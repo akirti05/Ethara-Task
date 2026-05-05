a0907f0649df1e5501ff199b12c6351996fa65a6
Akshaya Kirti <akshayakirti@Akshayas-MacBook-Air-3.local>
2026-05-05 14:38:14 +0530

    fix task controller

--- src/modules/tasks/task.controller.js
+++ src/modules/tasks/task.controller.js
@@ -1,32 +1,68 @@
 const taskService = require('./task.service');
 const { success, paginated } = require('../../utils/response');
 
+// CREATE TASK
 const create = async (req, res, next) => {
   try {
-    const task = await taskService.create(req.body, req.user.id);
+    const task = await taskService.create(
+      {
+        title: req.body.title,
+        description: req.body.description,
+        projectId: req.params.projectId // 🔥 important fix
+      },
+      req.user.id
+    );
+
     success(res, task, 201);
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
 
+// GET TASKS (by project)
 const list = async (req, res, next) => {
   try {
-    const { tasks, total } = await taskService.list(req.params.projectId, req.query, req.user.id);
+    const { tasks, total } = await taskService.list(
+      req.params.projectId,
+      req.query,
+      req.user.id
+    );
+
     paginated(res, tasks, total, req.query.page, req.query.limit);
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
 
+// UPDATE TASK
 const update = async (req, res, next) => {
   try {
-    const task = await taskService.update(req.params.taskId, req.body, req.user.id);
+    const task = await taskService.update(
+      req.params.taskId,
+      req.body,
+      req.user.id
+    );
+
     success(res, task);
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
 
+// DELETE TASK
 const remove = async (req, res, next) => {
   try {
     await taskService.remove(req.params.taskId, req.user.id);
+
     success(res, { message: 'Task deleted' });
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports = { create, list, update, remove };
+module.exports = {
+  create,
+  list,
+  update,
+  remove
+};
